d2c0175ace1d79eaa23b7ceaaa0fec7846985261
Oscar Bedford <otbedford@madisoncollege.edu>
2021-09-30 23:03:53 -0500

    Developing Customer Object

--- src/main/java/com/mistertoo/pizzawebsite/entity/Order.java
+++ src/main/java/com/mistertoo/pizzawebsite/entity/Order.java
@@ -1,4 +1,18 @@
 package com.mistertoo.pizzawebsite.entity;
+import javax.persistence.*;
+import org.hibernate.*;
+import org.hibernate.annotations.GenericGenerator;
 
+@Entity(name="order")
+@Table(name="orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO,generator = "native")
+    @GenericGenerator(name="native",strategy = "native")
+    private int ID;
+    private String size;
+    private String toppings;
+    private boolean pickup;
+    private String address;
+    private int CustomerID;
 }
--- src/main/java/com/mistertoo/pizzawebsite/persistence/OrderDAO.java
+++ src/main/java/com/mistertoo/pizzawebsite/persistence/OrderDAO.java
@@ -1,4 +1,12 @@
 package com.mistertoo.pizzawebsite.persistence;
 
+
+import com.mistertoo.pizzawebsite.entity.Customer;
+import org.apache.logging.log4j.*;
+import org.hibernate.*;
+import javax.persistence.*;
+import javax.persistence.criteria.*;
+import java.util.List;
+
 public class OrderDAO {
 }
