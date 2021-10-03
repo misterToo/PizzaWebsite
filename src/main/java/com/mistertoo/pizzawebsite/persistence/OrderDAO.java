544eb374bef28e228930ecddc5c4f5b985650b85
Oscar Bedford <otbedford@madisoncollege.edu>
2021-10-03 17:39:47 -0500

    Adding to orderDAO and starting test class

--- src/main/java/com/mistertoo/pizzawebsite/persistence/OrderDAO.java
+++ src/main/java/com/mistertoo/pizzawebsite/persistence/OrderDAO.java
@@ -1,7 +1,6 @@
 package com.mistertoo.pizzawebsite.persistence;
 
 
-import com.mistertoo.pizzawebsite.entity.Customer;
 import org.apache.logging.log4j.*;
 import org.hibernate.*;
 import javax.persistence.*;
@@ -11,4 +10,36 @@ import java.util.List;
 public class OrderDAO {
     private final Logger logger = LogManager.getLogger(this.getClass());
     SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
+    public Order getByOrderID(int id){
+        Session session = sessionFactory.openSession();
+        Order order = session.get(Order.class, id);
+        session.close();
+        return order;
+    }
+    public void update(Order order){
+        Session session = sessionFactory.openSession();
+        session.saveOrUpdate(order);
+        session.close();
+    }
+    public void delete(Order order){
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(order);
+        transaction.commit();
+        session.close();
+    }
+    public List<Order> getAll(){
+        Session session = sessionFactory.openSession();
+
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Order> query = builder.createQuery(Order.class);
+        Root<Order> root = query.from(Order.class);
+        List<Order> orders = session.createQuery(query).getResultList();
+
+        logger.debug("The list of Customers: " + orders);
+        session.close();
+        return orders;
+
+    }
 }
---
+++ src/main/java/test/java/OrderDAOTest.java
@@ -0,0 +1,10 @@
+package test.java;
+import com.mistertoo.pizzawebsite.entity.Order;
+import com.mistertoo.pizzawebsite.persistence.OrderDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+public class OrderDAOTest {
+    OrderDAO dao;
+}
