bd5411825f761498fa7cfaad5d388d247a0c56e8
Oscar Bedford <otbedford@madisoncollege.edu>
2021-11-06 22:12:51 -0500

    whoops

--- src/main/java/com/mistertoo/pizzawebsite/controller/displayCustomer.java
+++ src/main/java/com/mistertoo/pizzawebsite/controller/displayCustomer.java
@@ -19,7 +19,6 @@ public class displayCustomer extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         CustomerDAO dao = new CustomerDAO();
-        String committed = "commit";
         req.setAttribute("customers", dao.getAll());
         req.setAttribute("title", "All Customers: ");
         RequestDispatcher dispatcher = req.getRequestDispatcher("/results.jsp");
