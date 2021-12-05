4f79b88e0c92eec509d5ce510e40e2b2e2cc17d3
Oscar Bedford <otbedford@madisoncollege.edu>
2021-12-04 21:03:45 -0600

    Added account delete functionality

--- src/main/java/com/mistertoo/pizzawebsite/controller/DeleteAccount.java
+++ src/main/java/com/mistertoo/pizzawebsite/controller/DeleteAccount.java
@@ -9,7 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.*;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(
         urlPatterns = {"/deleteAccount"}
@@ -19,6 +22,14 @@ public class DeleteAccount extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         GenericDao<Customer> dao = new GenericDao<>(Customer.class);
-
+        HttpSession session = req.getSession(false);
+        String userName = (String) session.getAttribute("userName");
+        Map<String, Object> propertyMap = new HashMap<>();
+        propertyMap.put("uName", userName);
+        Customer currentCustomer = dao.findByPropertyEqual(propertyMap).get(0);
+        dao.delete(currentCustomer);
+        session.invalidate();
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/index.jsp");
+        dispatcher.forward(req, resp);
     }
 }
